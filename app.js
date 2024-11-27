b578db2d1221d6cc6d39819ca1633af382ce31bc
Zur_shani <zurshani12@gmail.com>
2024-11-27 22:04:08 +0200

    mongoose

--- app.js
+++ app.js
@@ -1,15 +1,42 @@
 const express = require("express");
-const app = express();
-const dotenv = require("dotenv").config();
-const port = process.env.PORT;
+const mongoose = require("mongoose");
+require("dotenv").config();
 
+const app = express();
+const port = process.env.PORT || 3000;
+
+// MongoDB Connection
+const dbConnect =
+  process.env.DB_CONNECT || "mongodb://localhost:27017/ExampleDB";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(dbConnect, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      connectTimeoutMS: 10000,
+    });
+    console.log("Connected to MongoDB successfully");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+    process.exit(1); // Exit process with failure
+  }
+};
+
+connectDB();
+
+// Routes
 app.get("/", (req, res) => {
   res.send("Hello World!!!!");
 });
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const postsRoutes = require("./routes/posts_routes");
 app.use("/posts", postsRoutes);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,13 +1,52 @@
+const Posts = require("../models/posts_model");
+const { ObjectId } = require("mongodb");
 const createPost = (req, res) => {
-  res.send("Create Post");
+  console.log(req.body);
+  Posts.create(req.body)
+    .then((post) => {
+      res.status(201).json(post);
+    })
+    .catch((error) => {
+      res.status(400).json(error);
+    });
 };
 
-const getAllPosts = (req, res) => {
-  res.send("Get All Posts");
+const getAllPosts = async (req, res) => {
+  const filter = req.query;
+  console.log(filter);
+  try {
+    if (filter.owner) {
+      const posts = await Posts.find({ owner: filter.owner });
+      return res.status(200).json(posts);
+    } else {
+      const posts = await Posts.find();
+      res.status(200).json(posts);
+    }
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+
+const getPostById = async (req, res) => {
+  const id = req.params.id;
+  if (id) {
+    try {
+      const post = await Posts.findById(id);
+      if (post) {
+        return res.send(post);
+      } else {
+        return res.status(404).send("Post not found");
+      }
+    } catch (error) {
+      return res.status(400).send(error.message);
+    }
+  } else {
+    return res.status(400).send("Invalid ID");
+  }
 };
 
 const deletePost = (req, res) => {
   res.send("Delete Post");
 };
 
-module.exports = { createPost, getAllPosts, deletePost };
+module.exports = { createPost, getAllPosts, deletePost, getPostById };
